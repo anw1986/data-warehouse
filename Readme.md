d03cb76fc18b5686d4eb54815092e571de8d9235
ahmed <awaheed@utexas.edu>
2020-10-07 15:37:16 -0500

    completed the python scripts

--- Readme.md
+++ Readme.md
@@ -1,1 +1,22 @@
-*readme coming*
+# Data Warehouse
+
+## Introduction
+A music streaming startup, Sparkify, has grown their user base and song database and want to move their processes and data onto the cloud. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+Developed an ETL pipeline that extracts their AWS S3, stages them in Redshift, and transforms data into a set of fact and dimensional tables to perform analytics 
+
+## Main Processing Steps
+* A **dwh-sample.cfg** file is provided. After pulling the repo provide the following parameters in config file to run the **create_tables.py** **etl.py** DB_HOST, ARN, AWS Key & Secret.
+* Extract data from S3
+* Stage them in Redshift
+* Transform data into a set of dimensional tables (star schema) 
+
+## Database Design
+The database is designed using the Star Schema. The data after loading into the staging table is injested from the staging table to the target Fact and Dimension tables
+
+    Fact Table  |   Dimension Table
+----------------| -------------------
+songplay_table  |   songsong_table
+                |   artist_table
+                |   user_table
+                |   time_table
--- sql_queries.py
+++ sql_queries.py
@@ -7,60 +7,151 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events_log"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay_table"
+user_table_drop = "DROP TABLE IF EXISTS user_table"
+song_table_drop = "DROP TABLE IF EXISTS song_table"
+artist_table_drop = "DROP TABLE IF EXISTS artist_table"
+time_table_drop = "DROP TABLE IF EXISTS time_table"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+    CREATE TABLE staging_events_log(
+    artist                  VARCHAR,
+    auth                    VARCHAR,
+    firstName               VARCHAR,              
+    gender                  VARCHAR,
+    itemInSession           INTEGER,
+    lastName                VARCHAR,
+    length                  FLOAT,
+    level                   VARCHAR,
+    location                VARCHAR,
+    method                  VARCHAR,
+    page                    VARCHAR,
+    registration            FLOAT,
+    sessionId               INTEGER,
+    song                    VARCHAR,
+    status                  INTEGER,
+    ts                      TIMESTAMP,
+    userAgent               VARCHAR,
+    userId                  INTEGER
+);
 """)
 
 staging_songs_table_create = ("""
+    CREATE TABLE staging_songs(
+        num_songs           INTEGER,
+        artist_id           VARCHAR,
+        artist_latitude     FLOAT,
+        artist_longitude    FLOAT,
+        artist_location     VARCHAR,
+        artist_name         VARCHAR,
+        song_id             VARCHAR,
+        title               VARCHAR,
+        duration            FLOAT,
+        year                INTEGER
+    );
 """)
 
 songplay_table_create = ("""
+    CREATE TABLE songplay_table(
+        songplay_id     INT IDENTITY(0,1), 
+        start_time      VARCHAR, 
+        user_id         INTEGER, 
+        level           VARCHAR, 
+        song_id         VARCHAR, 
+        artist_id       VARCHAR, 
+        session_id      INTEGER, 
+        location        VARCHAR, 
+        user_agent      VARCHAR
+    );
 """)
 
 user_table_create = ("""
+    CREATE TABLE user_table(
+        user_id     INTEGER, 
+        first_name  VARCHAR, 
+        last_name   VARCHAR, 
+        gender      VARCHAR, 
+        level       VARCHAR
+    );
 """)
 
 song_table_create = ("""
+    CREATE TABLE song_table(
+        song_id     VARCHAR, 
+        title       VARCHAR, 
+        artist_id   VARCHAR, 
+        year        INT,
+        duration    FLOAT
+    );
 """)
 
 artist_table_create = ("""
+    CREATE TABLE artist_table(
+        artist_id   VARCHAR,
+        name        VARCHAR, 
+        location    VARCHAR, 
+        latitude    FLOAT, 
+        longitude   FLOAT
+    );
 """)
 
 time_table_create = ("""
+    CREATE TABLE time_table(
+        start_time      TIMESTAMP,
+        time_hour       INTEGER, 
+        time_day        INTEGER, 
+        time_week       INTEGER, 
+        time_month      INTEGER, 
+        time_year       INTEGER, 
+        time_weekday    INTEGER
+    );
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+    copy staging_events_log 
+    from {} \
+    iam_role {}\
+    FORMAT AS json {}\
+    TIMEFORMAT 'epochmillisecs';
+""").format(config['S3']['LOG_DATA'],config['IAM_ROLE']['ARN'],config['S3']['LOG_JSONPATH'])
 
 staging_songs_copy = ("""
-""").format()
+    copy staging_songs 
+    from {} \
+    iam_role {}
+    json 'auto';
+""").format(config['S3']['SONG_DATA'],config['IAM_ROLE']['ARN'])
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
+    INSERT INTO songplay_table (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) SELECT ts, userId, level, song_id, artist_id, sessionId, location, userAgent FROM staging_events_log se, staging_songs ss 
+    WHERE se.page='NextSong' 
+    AND se.song = ss.title
+    AND se.artist = ss.artist_name
+    AND se.length = ss.duration
 """)
 
 user_table_insert = ("""
+    INSERT INTO user_table SELECT userId, firstName, lastName, gender, level FROM staging_events_log
 """)
 
 song_table_insert = ("""
+    INSERT INTO song_table SELECT song_id, title, artist_id, year, duration FROM staging_songs
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artist_table SELECT artist_id, artist_name, artist_location, artist_latitude, artist_longitude FROM staging_songs
 """)
 
 time_table_insert = ("""
+    INSERT INTO time_table SELECT ts, EXTRACT(HOUR FROM ts), EXTRACT(DAY FROM ts), EXTRACT(WEEK FROM ts), EXTRACT(MONTH FROM ts), EXTRACT(YEAR FROM ts), EXTRACT(dayofweek FROM ts) FROM staging_events_log
 """)
 
 # QUERY LISTS
