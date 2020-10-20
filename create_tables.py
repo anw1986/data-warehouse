f2a75b1b89082c773c5b23f12667b854489aa675
ahmed <awaheed@utexas.edu>
2020-10-20 07:25:46 -0500

    Updated the comments and the Primary Key in the tables

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,26 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Drops table in the data base"""
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Create tables in the database"""
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    The main function calls the create_tables and drop_tables function
+    The database parameters are in the config file and are used to connect to the database
+    The connection (conn) and cursor (cur) are passed as parameters to the drop_table and create_table
+    
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -4,18 +4,28 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Function used to injest data in the staging table"""
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """Function used to injest data into the fact and dimension table from the staging table"""
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    The main function calls the load_staging_tables and insert_tables function
+    The database parameters are in the config file and are used to connect to the database
+    The connection (conn) and cursor (cur) are passed as parameters to the load_staging_tables and insert_tables function
+    Data is then populated into the staging tables and then the target tables
+
+    """
+
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
